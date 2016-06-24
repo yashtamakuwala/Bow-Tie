e53f1f4e1d0c8732d1b71e24210eaf46159c3814
yashtamakuwala <neel64tamakuwala@gmail.com>
2016-06-24 18:25:51 +0530

    Initial Commit

---
+++ Bowtie+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Bowtie+CoreDataProperties.swift
+//  Bow Ties
+//
+//  Created by YASH on 24/06/16.
+//  Copyright © 2016 Razeware. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Bowtie {
+
+    @NSManaged var name: String?
+    @NSManaged var isFavourite: NSNumber?
+    @NSManaged var lastWorn: NSDate?
+    @NSManaged var rating: NSNumber?
+    @NSManaged var searchKey: String?
+    @NSManaged var timesWorn: NSNumber?
+    @NSManaged var photoData: NSData?
+    @NSManaged var tintColor: NSObject?
+
+}
---
+++ Bowtie.swift
@@ -0,0 +1,17 @@
+//
+//  Bowtie.swift
+//  Bow Ties
+//
+//  Created by YASH on 24/06/16.
+//  Copyright © 2016 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Bowtie: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
